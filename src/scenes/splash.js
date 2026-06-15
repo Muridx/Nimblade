3c49c2e55b92673edde231b0f0d18a69f2cbad9f
Muridx <atokahmad236@gmail.com>
2026-06-15 07:22:36 +0700

    feat: HP/energy persist, NIM->gem gauntlet, Hub dev wallet, 2-line CTA buttons

--- src/scenes/splash.js
+++ src/scenes/splash.js
@@ -3,7 +3,7 @@ import { mountScene } from "./sceneManager.js";
 export function splashScene(root) {
   root.innerHTML = `
     <div class="splash">
-      <div class="splash__logo" role="img" aria-label="NIMBLADE"></div>
+      <div class="" role="img" aria-label="NIMBLADE"></div>
       <p class="splash__cta"></p>
     </div>
   `;
